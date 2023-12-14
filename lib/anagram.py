b96dc324bf256d8cbf8ebe967f57ef7901569ca6
Connor Burns <cpburns17@gmail.com>
2023-12-14 15:16:03 -0500

    barely made it

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word = '') -> None:
+        self.word = word
+        
+
+
+    def match(self, word_list):
+        match_list = []
+        for word in word_list:
+            if sorted(word) == sorted(self.word):
+                match_list.append(word)
+        return match_list
+                
+
+
+print(Anagram("enlist").match)
+
